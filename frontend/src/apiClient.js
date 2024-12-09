20f0ac56198f12eb97727a24f0e8c226003e2cef
dada-mhmd <moudi.ld@hotmail.com>
2024-12-09 12:51:20 +0200

    fix issues

--- frontend/src/apiClient.js
+++ frontend/src/apiClient.js
@@ -1,4 +1,4 @@
-const API_BASE_URL =import.meta.env.VITE_NODE_ENV === 'development' ? 'http://localhost:5000' : '/api';
+const API_BASE_URL =import.meta.env.VITE_NODE_ENV === 'development' ? 'http://localhost:5000' : '';
 
 export const register = async (formData) => {
   const res = await fetch(`${API_BASE_URL}/api/users/register`, {
